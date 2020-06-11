6e4d842113c41ac072c1d6eac499dc6df9436cf3
Ujoy7851 <joy7851@gmail.com>
2020-06-11 15:42:51 +0900

    update main.js

--- main.js
+++ main.js
@@ -2,35 +2,37 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const qs = require("querystring");
+const path = require("path");
 
-function templateHTML(title, list, body, control) {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-        <title>WEB - ${title}</title>
+let template = {
+    HTML:(title, list, body, control) => {
+      return `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
         <meta charset="utf-8">
-    </head>
-    <body>
+      </head>
+      <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
         ${control}
         ${body}
-    </body>
-    </html>
-    `;
-}
-
-function templateList(filelist) {
-    let list = "<ul>";
-    let i = 0;
-    while (i < filelist.length) {
+      </body>
+      </html>
+      `;
+    },
+    list: (filelist) => {
+      let list = '<ul>';
+      let i = 0;
+      while(i < filelist.length){
         list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
         i = i + 1;
+      }
+      list = list+'</ul>';
+      return list;
     }
-    list = list + "</ul>";
-    return list;
-}
+  }
 
 const app = http.createServer((request, response) => {
     let _url = request.url;
@@ -42,27 +44,36 @@ const app = http.createServer((request, response) => {
             fs.readdir("./data", (error, filelist) => {
                 let title = "Welcome";
                 let description = "Hello, Node.js";
-                let list = templateList(filelist);
-                let template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+                let list = template.list(filelist);
+                let html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
                 response.writeHead(200);
-                response.end(template);
+                response.end(html);
             });
         } else {
             fs.readdir("./data", (error, filelist) => {
-                fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+                let filteredId = path.parse(queryData.id).base;
+                fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
                     let title = queryData.id;
-                    let list = templateList(filelist);
-                    let template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                    let list = template.list(filelist);
+                    let html = template.HTML(title, list,
+                        `<h2>${title}</h2>${description}`, 
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="/delete_process" method="post">
+                            <input type="hidden" name="id" value=${title}>
+                            <input type="submit" value="delete">
+                        </form>
+                        `);
                     response.writeHead(200);
-                    response.end(template);
+                    response.end(html);
                 });
             });
         }
     } else if (pathname === "/create") {
         fs.readdir("./data", (error, filelist) => {
             let title = "WEB - create";
-            let list = templateList(filelist);
-            let template = templateHTML(title, list,
+            let list = template.list(filelist);
+            let html = template.HTML(title, list,
                 `
                 <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
@@ -76,7 +87,7 @@ const app = http.createServer((request, response) => {
                 `, ''
             );
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
         });
     } else if(pathname === '/create_process') {
         let body = '';
@@ -95,10 +106,11 @@ const app = http.createServer((request, response) => {
         }); 
     } else if(pathname === '/update') {
         fs.readdir("./data", (error, filelist) => {
-            fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
+            let filteredId = path.parse(queryData.id).base;
+            fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
                 let title = queryData.id;
-                let list = templateList(filelist);
-                let template = templateHTML(title, list,
+                let list = template.list(filelist);
+                let html = template.HTML(title, list,
                     `
                     <form action="/update_process" method="post">
                         <input type="hidden" name="id" value=${title}>
@@ -112,7 +124,7 @@ const app = http.createServer((request, response) => {
                     </form>
                     `, '');
                 response.writeHead(200);
-                response.end(template);
+                response.end(html);
             });
         });
     } else if(pathname === '/update_process') {
@@ -132,6 +144,21 @@ const app = http.createServer((request, response) => {
                     response.end();
                 });
             })
+        });
+    } else if(pathname === '/delete_process') {
+        let body = '';
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        request.on('end', () => {
+            let post = qs.parse(body);
+            let id = post.id;
+            let filteredId = path.parse(id).base;
+            fs.unlink(`data/${filteredId}`, (error) => {
+                response.writeHead(302, {Location: '/'});
+                response.end();
+            });
         });
     } else {
         response.writeHead(404);
